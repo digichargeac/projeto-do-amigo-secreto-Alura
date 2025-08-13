c828126510b27fb4b7ba94e0dbf3b39ed2214867
Antônio Carlos Martins Teixeira <digicharge@yahoo.com.br>
2025-08-13 16:56:13 -0300

    Função Adicionar Item a lista e Visualizar a Lista

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigosX = [];
+
+
+function adicionarAmigo() {
+    if (document.getElementById("amigo").value === "") {
+        alert("Por favor, insira o nome do amigo.");}
+    else {
+        amigosX.push(document.getElementById("amigo").value);
+        let novoItemLista = document.createElement("li");
+        novoItemLista.textContent = document.getElementById("amigo").value;
+        document.getElementById("listaAmigos").appendChild(novoItemLista);
+    }
+}
+
+
+
